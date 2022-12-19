25be818b17233fa9a8c5279188c58db1815f2055
mfirdausbn <mfirdausbn@gmail.com>
2022-12-19 12:24:41 +0800

    update

--- script.js
+++ script.js
@@ -48,42 +48,141 @@ document.addEventListener("keyup", moveFrog);
 ////////////////////////////////////////////////////////////////
 //CREATE FUNCTION TO MOVE LOGS
 ////////////////////////////////////////////////////////////////
-//each log now has a class of l1 to l5. l1,l2 and l3, will have a brown colour
-// l4 and l5 will have a a blue back ground.
-// how the logs move is by "cycling" the classes from l1 to l5 so that it changes from brown to blue
+// each log now has a class of l1 to l5. l1,l2 and l3, will have a brown colour
+// l4 and l5 will have a blue back ground.
+// logs move by "cycling" classes from l1 to l5 changing from brown to blue
 // giving the effect of it moving to the left when it changes from l3 to l4
+// do the reverse for logsright
 
 function autoMoveLogs() {
-  logsLeft.forEach((logleft) => moveLogLeft(logleft));
+  logsLeft.forEach((logLeft) => moveLogLeft(logLeft));
+  logsRight.forEach((logRight) => moveLogRight(logRight));
 }
 
-function moveLogLeft(logleft) {
+function moveLogLeft(logLeft) {
   switch (true) {
-    case logleft.classList.contains("l1"): //check if that square contains l1
-      logleft.classList.remove("l1"); //remove this l1 class
-      logleft.classList.add("l2"); //add this l2 class
+    case logLeft.classList.contains("l1"): //check if that square contains l1
+      logLeft.classList.remove("l1"); //remove this l1 class
+      logLeft.classList.add("l2"); //add this l2 class
       break;
 
-    case logleft.classList.contains("l2"):
-      logleft.classList.remove("l2");
-      logleft.classList.add("l3");
+    case logLeft.classList.contains("l2"):
+      logLeft.classList.remove("l2");
+      logLeft.classList.add("l3");
       break;
 
-    case logleft.classList.contains("l3"):
-      logleft.classList.remove("l3");
-      logleft.classList.add("l4");
+    case logLeft.classList.contains("l3"):
+      logLeft.classList.remove("l3");
+      logLeft.classList.add("l4");
       break;
 
-    case logleft.classList.contains("l4"):
-      logleft.classList.remove("l4");
-      logleft.classList.add("l5");
+    case logLeft.classList.contains("l4"):
+      logLeft.classList.remove("l4");
+      logLeft.classList.add("l5");
       break;
 
-    case logleft.classList.contains("l5"):
-      logleft.classList.remove("l5");
-      logleft.classList.add("l1");
+    case logLeft.classList.contains("l5"):
+      logLeft.classList.remove("l5");
+      logLeft.classList.add("l1");
       break;
   }
 }
 
-setInterval(autoMoveLogs, 1000);
+function moveLogRight(logRight) {
+  switch (true) {
+    case logRight.classList.contains("l1"): //check if that square contains l1
+      logRight.classList.remove("l1"); //remove this l1 class
+      logRight.classList.add("l5"); //add this l5 class
+      break;
+
+    case logRight.classList.contains("l2"):
+      logRight.classList.remove("l2");
+      logRight.classList.add("l1");
+      break;
+
+    case logRight.classList.contains("l3"):
+      logRight.classList.remove("l3");
+      logRight.classList.add("l2");
+      break;
+
+    case logRight.classList.contains("l4"):
+      logRight.classList.remove("l4");
+      logRight.classList.add("l3");
+      break;
+
+    case logRight.classList.contains("l5"):
+      logRight.classList.remove("l5");
+      logRight.classList.add("l4");
+      break;
+  }
+}
+
+setInterval(autoMoveLogs, 500);
+
+////////////////////////////////////////////////////////////////
+//CREATE FUNCTION FOR CARS TO MOVE
+////////////////////////////////////////////////////////////////
+
+function autoMoveCars() {
+  carsLeft.forEach((carLeft) => moveCarLeft(carLeft));
+  carsRight.forEach((carRight) => moveCarRight(carRight));
+}
+
+function moveCarLeft(carLeft) {
+  switch (true) {
+    case carLeft.classList.contains("c1"): //check if that square contains l1
+      carLeft.classList.remove("c1"); //remove this l1 class
+      carLeft.classList.add("c2"); //add this l2 class
+      break;
+
+    case carLeft.classList.contains("c2"):
+      carLeft.classList.remove("c2");
+      carLeft.classList.add("c3");
+      break;
+
+    case carLeft.classList.contains("c3"):
+      carLeft.classList.remove("c3");
+      carLeft.classList.add("c4");
+      break;
+
+    case carLeft.classList.contains("c4"):
+      carLeft.classList.remove("c4");
+      carLeft.classList.add("c5");
+      break;
+
+    case carLeft.classList.contains("c5"):
+      carLeft.classList.remove("c5");
+      carLeft.classList.add("c1");
+      break;
+  }
+}
+
+//   function moveCarRight(carRight) {
+//     switch (true) {
+//       case carRight.classList.contains("c1"): //check if that square contains l1
+//         carRight.classList.remove("c1"); //remove this l1 class
+//         carRight.classList.add("c5"); //add this l5 class
+//         break;
+
+//       case carRight.classList.contains("c2"):
+//         carRight.classList.remove("c2");
+//         carRight.classList.add("c1");
+//         break;
+
+//       case carRight.classList.contains("c3"):
+//         carRight.classList.remove("c3");
+//         carRight.classList.add("c2");
+//         break;
+
+//       case carRight.classList.contains("c4"):
+//         carRight.classList.remove("c4");
+//         carRight.classList.add("c3");
+//         break;
+
+//       case carRight.classList.contains("c5"):
+//         carRight.classList.remove("c5");
+//         carRight.classList.add("c4");
+//         break;
+//     }
+//   }
+setInterval(autoMoveCars, 500);
